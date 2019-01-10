d90e8b964323c6c76f080a71ae08870450912a33
吴姓周先生 <jackking704@gmail.com>
2019-01-10 11:54:46 +0800

    Ex01 答案

--- Ex01.java
+++ Ex01.java
@@ -4,8 +4,30 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Ex01 {
     public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        Point2D[] points = new Point2D[N];
+        for (int i = 0; i < N; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            Point2D p = new Point2D(x, y);
+            points[i] = p;
+        }
+
+        double minDistance = 100;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                double distance = points[i].distanceTo(points[j]);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+            }
+        }
+        StdOut.println("min:" + minDistance);
 
     }
 }
